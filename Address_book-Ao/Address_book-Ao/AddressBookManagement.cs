13307157a19924f1cb84998df4029d0f845faa5b
“Aniket_D” <aniketdakhare@gmail.com>
2021-05-31 23:29:55 +0530

    [Nilima]Added Connection string

---
+++ Address_book-Ao/Address_book-Ao/AddressBookManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book_Ao
+{
+    /// <summary>
+    /// For Maneging data of address book
+    /// connection Establishment
+    /// </summary>
+    class AddressBookManagement
+    {
+        //Connection string for the connection to the database
+        public static string connectionString = @"Data Source=DESKTOP-DL043RM;Initial Catalog=AddressBookDataBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // SqlConnection=Open connection to the sql server database
+        //Initialised new instance of sqlConnection and pass connection string in that connection
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        //Adding method for databse connection 
+        public void DataBaseConnection()
+        {
+            try
+            {
+                //DtaTime is a instant of time
+                //It describes date and time of the day
+                //create object DateTime class 
+                //Now=local date and time
+                DateTime now = DateTime.Now;
+                connection.Open(); // open connection
+                using (connection)  //using SqlConnection
+                {
+                    Console.WriteLine($"Connection is created Successful {now}"); //print msg
+
+                }
+                connection.Close(); //close connection
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Address_book-Ao/Address_book-Ao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book_Ao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********WELCOME TO THE ADRESSBOOK ADO*********");
+
+            AddressBookManagement addressBookManagement = new AddressBookManagement();
+
+            addressBookManagement.DataBaseConnection(); //UC1
+            Console.ReadLine();
+           
+        }
+    }
+}
